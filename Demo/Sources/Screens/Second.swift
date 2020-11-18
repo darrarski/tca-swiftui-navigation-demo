2ea370b01ea83d9cecfad7c4a7c8237143577cff
Dariusz Rybicki <darrarski@gmail.com>
2020-11-18 23:26:44 +0100

    Add timer to Second screen

--- Demo/Sources/Screens/Second.swift
+++ Demo/Sources/Screens/Second.swift
@@ -2,11 +2,15 @@ import ComposableArchitecture
 import SwiftUI
 
 struct SecondState: Equatable {
+  let timerId = UUID()
+  var date: Date?
   var isPresentingThird = false
   var third: ThirdState?
 }
 
 enum SecondAction: Equatable {
+  case didAppear
+  case didTimerTick
   case presentThird(Bool)
   case third(ThirdAction)
   case didDisappear
@@ -20,6 +24,16 @@ let secondReducer = Reducer<SecondState, SecondAction, AppEnvironment>.combine(
   ),
   Reducer { state, action, environment in
     switch action {
+    case .didAppear:
+      return Effect.timer(id: state.timerId, every: .seconds(1), on: environment.mainScheduler)
+        .map { _ in .didTimerTick }
+        .prepend(.didTimerTick)
+        .eraseToEffect()
+
+    case .didTimerTick:
+      state.date = environment.currentDate()
+      return .none
+
     case let .presentThird(present):
       state.isPresentingThird = present
       if present {
@@ -37,49 +51,64 @@ let secondReducer = Reducer<SecondState, SecondAction, AppEnvironment>.combine(
       return .none
 
     case .didDisappear:
-      return .none
+      return .cancel(id: state.timerId)
     }
   }
 )
 
 struct SecondViewState: Equatable {
+  let date: Date?
   let isPresentingThird: Bool
 
   init(state: SecondState) {
+    date = state.date
     isPresentingThird = state.isPresentingThird
   }
 }
 
 struct SecondView: View {
   let store: Store<SecondState, SecondAction>
+  @Environment(\.timeFormatter) var timeFormatter
+
+  init(store: Store<SecondState, SecondAction>) {
+    self.store = store
+  }
 
   var body: some View {
     WithViewStore(store.scope(state: SecondViewState.init(state:))) { viewStore in
       ZStack {
         Color.green.ignoresSafeArea()
 
-        NavigationLink(
-          destination: IfLetStore(
-            store.scope(
-              state: \.third,
-              action: SecondAction.third
-            ),
-            then: ThirdView.init(store:)
-          ),
-          isActive: viewStore.binding(
-            get: \.isPresentingThird,
-            send: SecondAction.presentThird
-          ),
-          label: {
-            Text("Present Third")
+        VStack {
+          if let date = viewStore.date {
+            Text(timeFormatter.string(for: date))
               .padding()
           }
-        )
+
+          NavigationLink(
+            destination: IfLetStore(
+              store.scope(
+                state: \.third,
+                action: SecondAction.third
+              ),
+              then: ThirdView.init(store:)
+            ),
+            isActive: viewStore.binding(
+              get: \.isPresentingThird,
+              send: SecondAction.presentThird
+            ),
+            label: {
+              Text("Present Third")
+                .padding()
+            }
+          )
+        }
         .padding()
         .background(Color.primary.colorInvert())
       }
       .navigationTitle("Second")
       .navigationBarTitleDisplayMode(.inline)
+      .onAppear { viewStore.send(.didAppear) }
       .onDisappear { viewStore.send(.didDisappear) }
     }
   }
