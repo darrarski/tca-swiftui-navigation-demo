28a4bc0a3221a8cee3b8e6ba11667003de38c9ec
Dariusz Rybicki <darrarski@gmail.com>
2020-11-19 22:41:38 +0100

    Add DemoApp component

--- Demo/Sources/App/DemoApp.swift
+++ Demo/Sources/App/DemoApp.swift
@@ -2,24 +2,54 @@ import Combine
 import ComposableArchitecture
 import SwiftUI
 
+struct DemoAppState: Equatable {
+  var first: FirstState
+  var isRunningTests: Bool = ProcessInfo.processInfo.environment["XCTestConfigurationFilePath"] != nil
+}
+
+enum DemoAppAction: Equatable {
+  case first(FirstAction)
+}
+
+let demoAppReducer = Reducer<DemoAppState, DemoAppAction, AppEnvironment>.combine(
+  firstReducer.pullback(
+    state: \.first,
+    action: /DemoAppAction.first,
+    environment: { $0 }
+  )
+)
+
+struct DemoAppViewState: Equatable {
+  let isPresentingUI: Bool
+
+  init(state: DemoAppState) {
+    isPresentingUI = state.isRunningTests == false
+  }
+}
+
 @main
 struct DemoApp: App {
+  let store: Store<DemoAppState, DemoAppAction> = Store(
+    initialState: DemoAppState(
+      first: FirstState()
+    ),
+    reducer: demoAppReducer,
+    environment: AppEnvironment()
+  )
+
   var body: some Scene {
     WindowGroup {
-      if isRunningTests == false {
-        NavigationView {
-          FirstView(store: Store(
-            initialState: FirstState(),
-            reducer: firstReducer.debug(),
-            environment: AppEnvironment()
-          ))
+      WithViewStore(store.scope(state: DemoAppViewState.init(state:))) { viewStore in
+        if viewStore.isPresentingUI {
+          NavigationView {
+            FirstView(store: store.scope(
+              state: \.first,
+              action: DemoAppAction.first
+            ))
+          }
+          .navigationViewStyle(StackNavigationViewStyle())
         }
-        .navigationViewStyle(StackNavigationViewStyle())
       }
     }
-  }
-
-  var isRunningTests: Bool {
-    ProcessInfo.processInfo.environment["XCTestConfigurationFilePath"] != nil
   }
 }
