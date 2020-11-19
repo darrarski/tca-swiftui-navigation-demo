70424859fe7c6623d05b11b836cede4022e73b0b
Dariusz Rybicki <darrarski@gmail.com>
2020-11-19 21:55:59 +0100

    Add tests for fetch on second screen

--- Demo/Tests/Screens/SecondTests.swift
+++ Demo/Tests/Screens/SecondTests.swift
@@ -1,5 +1,6 @@
 import XCTest
 @testable import Demo
+import Combine
 import ComposableArchitecture
 
 final class SecondTests: XCTestCase {
@@ -52,6 +53,44 @@ final class SecondTests: XCTestCase {
       },
       .send(.third(.didDisappear)) {
         $0.third = nil
+      }
+    )
+  }
+
+  func testFetch() {
+    let idStub = UUID()
+    let fetcher = PassthroughSubject<Date, Never>()
+    let store = TestStore(
+      initialState: SecondState(
+        fetchId: idStub
+      ),
+      reducer: secondReducer,
+      environment: AppEnvironment(
+        randomId: { fatalError() },
+        fetcher: { fetcher.first().eraseToAnyPublisher() },
+        timer: { fatalError() }
+      )
+    )
+
+    store.assert(
+      .send(.didAppear),
+      .receive(.fetchDate),
+      .do {
+        fetcher.send(Date(timeIntervalSince1970: 0))
+      },
+      .receive(.didFetchDate(Date(timeIntervalSince1970: 0))) {
+        $0.fetchedDate = Date(timeIntervalSince1970: 0)
+      },
+      .receive(.fetchDate),
+      .do {
+        fetcher.send(Date(timeIntervalSince1970: 1))
+      },
+      .receive(.didFetchDate(Date(timeIntervalSince1970: 1))) {
+        $0.fetchedDate = Date(timeIntervalSince1970: 1)
+      },
+      .receive(.fetchDate),
+      .do {
+        fetcher.send(completion: .finished)
       }
     )
   }
