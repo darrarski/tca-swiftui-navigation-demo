b818912a4d78c37e872e80cd7fe82332d6be9624
Dariusz Rybicki <darrarski@gmail.com>
2020-11-19 23:31:55 +0100

    Add demo app navigation tests

---
+++ Demo/Tests/App/DemoAppTests.swift
@@ -0,0 +1,107 @@
+import XCTest
+@testable import Demo
+import Combine
+import ComposableArchitecture
+
+final class DemoAppTests: XCTestCase {
+  func testPushSecondPushThirdPopThirdPopSecond() {
+    var latestId: UUID!
+    let store = TestStore(
+      initialState: DemoAppState(
+        first: FirstState(),
+        isRunningTests: false
+      ),
+      reducer: demoAppReducer,
+      environment: AppEnvironment(
+        randomId: {
+          latestId = UUID()
+          return latestId
+        },
+        fetcher: {
+          Empty(completeImmediately: false).eraseToAnyPublisher()
+        },
+        timer: {
+          Empty(completeImmediately: false).eraseToAnyPublisher()
+        }
+      )
+    )
+
+    store.assert(
+      .send(.first(.presentSecond(true))) {
+        $0.first.isPresentingSecond = true
+        $0.first.second = SecondState()
+      },
+      .send(.first(.second(.didAppear))) {
+        $0.first.second!.fetchId = latestId
+      },
+      .receive(.first(.second(.fetchDate))),
+      .send(.first(.second(.presentThird(true)))) {
+        $0.first.second!.isPresentingThird = true
+        $0.first.second!.third = ThirdState()
+      },
+      .send(.first(.second(.third(.didAppear)))) {
+        $0.first.second!.third!.timerId = latestId
+      },
+      .send(.first(.second(.presentThird(false)))) {
+        $0.first.second!.isPresentingThird = false
+      },
+      .send(.first(.second(.third(.didDisappear)))) {
+        $0.first.second!.third = nil
+      },
+      .send(.first(.presentSecond(false))) {
+        $0.first.isPresentingSecond = false
+      },
+      .send(.first(.second(.didDisappear))) {
+        $0.first.second = nil
+      }
+    )
+  }
+
+  func testPushSecondPushThirdPopToFirst() {
+    var latestId: UUID!
+    let store = TestStore(
+      initialState: DemoAppState(
+        first: FirstState(),
+        isRunningTests: false
+      ),
+      reducer: demoAppReducer,
+      environment: AppEnvironment(
+        randomId: {
+          latestId = UUID()
+          return latestId
+        },
+        fetcher: {
+          Empty(completeImmediately: false).eraseToAnyPublisher()
+        },
+        timer: {
+          Empty(completeImmediately: false).eraseToAnyPublisher()
+        }
+      )
+    )
+
+    store.assert(
+      .send(.first(.presentSecond(true))) {
+        $0.first.isPresentingSecond = true
+        $0.first.second = SecondState()
+      },
+      .send(.first(.second(.didAppear))) {
+        $0.first.second!.fetchId = latestId
+      },
+      .receive(.first(.second(.fetchDate))),
+      .send(.first(.second(.presentThird(true)))) {
+        $0.first.second!.isPresentingThird = true
+        $0.first.second!.third = ThirdState()
+      },
+      .send(.first(.second(.third(.didAppear)))) {
+        $0.first.second!.third!.timerId = latestId
+      },
+      .send(.first(.second(.third(.dismissToFirst)))),
+      .receive(.first(.presentSecond(false))) {
+        $0.first.isPresentingSecond = false
+      },
+      .send(.first(.second(.third(.didDisappear)))) {
+        $0.first.second = nil
+      }
+    )
+  }
+}
