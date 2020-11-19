1f8f42da37c0bc3cd9433af27e8fa0b5358f49da
Dariusz Rybicki <darrarski@gmail.com>
2020-11-19 23:40:48 +0100

    Add TimeFormatter.preview

--- Demo/Sources/Formatters/TimeFormatter.swift
+++ Demo/Sources/Formatters/TimeFormatter.swift
@@ -10,6 +10,16 @@ struct TimeFormatter {
   }
 }
 
+extension TimeFormatter {
+  static var preview: Self {
+    let formatter = TimeFormatter()
+    formatter.dateFormatter.calendar = Calendar(identifier: .gregorian)
+    formatter.dateFormatter.locale = Locale(identifier: "en_US")
+    formatter.dateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
+    return formatter
+  }
+}
+
 extension EnvironmentValues {
   struct TimeFormatterKey: EnvironmentKey {
     static var defaultValue = TimeFormatter()
